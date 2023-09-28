1e0855cc76f485b9df6f669c15df33fd047058b0
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-09-28 16:12:31 +0530

    Added: Post the new Tours.

--- natours/app.js
+++ natours/app.js
@@ -5,6 +5,10 @@ const fs = require('fs');
 // Setting up to the app variable to call others.
 const app = express();
 
+//MiddleWares
+// For use the body in request.
+app.use(express.json());
+
 // Setting up the port.
 const port = 8080;
 
@@ -27,7 +31,7 @@ app.post('/', (req, res) => {
 }); */
 // Fetch The Data from file
 
-const fileData = fs.readFileSync('./dev-data/data/tours.json', 'utf-8');
+const fileData = fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8');
 const tours = JSON.parse(fileData);
 
 // Get the Tours List
@@ -35,6 +39,20 @@ app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
+});
+
+// Post a New Tour
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newObject = Object.assign({ id: newId }, req.body);
+  tours.push(newObject);
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: newObject });
+    }
+  );
 });
 
 // Listening to the server
