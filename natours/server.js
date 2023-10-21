7d03b003d731d3afbeeec443d5db6e50f4a2d4e4
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-21 14:43:51 +0530

    Added: Establish the connection of Mongoose.

--- natours/server.js
+++ natours/server.js
@@ -1,7 +1,20 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app');
+const app = require("./app");
+
+// Get the  Mongo DB URL Connection String
+const DB = process.env.DATABASE;
+
+// NO make a connection to DB
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`Connected To ${process.env.DATABASE} Successfully`));
 
 // Setting up the port.
 const port = process.env.PORT;
