6d68d04707623dc2596b3432d37391ba49adcda2
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-21 14:59:26 +0530

    Added: Use moongse to store the data in DB by creating schemas.

--- natours/server.js
+++ natours/server.js
@@ -16,6 +16,39 @@ mongoose
   })
   .then(() => console.log(`Connected To ${process.env.DATABASE} Successfully`));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A Tour must have a Name."],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "Tour must have a Price."],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 500,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("Error 🔥:", err);
+  });
+
 // Setting up the port.
 const port = process.env.PORT;
 // Listening to the server
