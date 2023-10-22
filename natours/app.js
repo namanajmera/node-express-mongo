a9b970fc6a0cbd96114f22acae50716531de8ce6
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-22 11:42:42 +0530

    Added: Handle the unknown routes.

--- natours/app.js
+++ natours/app.js
@@ -1,25 +1,25 @@
 // Imports
-const express = require('express');
-const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoute');
-const usersRouter = require('./routes/userRoute');
+const express = require("express");
+const morgan = require("morgan");
+const tourRouter = require("./routes/tourRoute");
+const usersRouter = require("./routes/userRoute");
 
 // Setting up to the app variable to call others.
 const app = express();
 
 //MiddleWares
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // For use the body in request.
 app.use(express.json());
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 // Custom Middleware
 app.use((req, res, next) => {
-  console.log('Hello, I am from middleware');
+  console.log("Hello, I am from middleware");
   next();
 });
 
@@ -29,7 +29,15 @@ app.use((req, res, next) => {
 });
 
 // All Routers
-app.use('/api/v1', tourRouter);
-app.use('/api/v1', usersRouter);
+app.use("/api/v1", tourRouter);
+app.use("/api/v1", usersRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on the server`,
+  });
+  next();
+});
 
 module.exports = app;
