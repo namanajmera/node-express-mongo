f74ba844a3eea8b4aaa328464623ed77cfdb95cd
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-22 11:55:50 +0530

    Added: Create the global Code.

--- natours/app.js
+++ natours/app.js
@@ -33,11 +33,24 @@ app.use("/api/v1", tourRouter);
 app.use("/api/v1", usersRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
+  /* res.status(404).json({
     status: "fail",
     message: `Can't find ${req.originalUrl} on the server`,
+  }); */
+  const err = new Error(`Can't find ${req.originalUrl} on the server`);
+  err.statusCode = 404;
+  err.status = "fail";
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 module.exports = app;
