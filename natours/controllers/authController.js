e61221e74970b3528fafbdeae458937ecfee47b0
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-23 17:48:45 +0530

    Added: Use the JWT to generate the token using the JWT.

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -1,11 +1,17 @@
 const createAsync = require("./../utils/createAsync");
 const User = require("./../models/userModal");
+const jwt = require("jsonwebtoken");
 
 exports.signup = createAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
