1f72312e4670817b2a53abe950b6ac9e5d548a6d
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-26 15:54:29 +0530

    Added: Embeded the users with tours.

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require("./userModal");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -81,6 +82,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -116,6 +118,12 @@ tourSchema.pre("aggregate", function (next) {
   this.pipeline().unshift({
     $match: { _id: { $ne: "easy" } },
   });
+  next();
+});
+
+tourSchema.pre("save", async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
