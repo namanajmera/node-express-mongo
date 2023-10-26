07a84f91b0039b9c8df4a298e691754acf53c3bd
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-26 16:03:21 +0530

    Added: Reference to the user collection.

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
-const User = require("./userModal");
+// const User = require("./userModal");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -82,7 +82,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -121,11 +126,12 @@ tourSchema.pre("aggregate", function (next) {
   next();
 });
 
-tourSchema.pre("save", async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// Embedded Users
+// tourSchema.pre("save", async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
