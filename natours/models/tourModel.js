82580affb4c3534b21ff2c61c2f101c56aeceaeb
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-10-26 16:20:39 +0530

    Added: Create a middleware in tourModel to populate the guides.

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -133,6 +133,14 @@ tourSchema.pre("aggregate", function (next) {
 //   next();
 // });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordResetExpires -passwordResetToken",
+  });
+  next();
+});
+
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
